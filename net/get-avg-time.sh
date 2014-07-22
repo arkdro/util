8cfd2a98e9947887968c761dabba7dfb9b891ca6
Arkadiy Drovosekov <arkdro@gmail.com>
2014-07-22 14:38:27 +0400

    print timestamp

--- net/get-avg-time.sh
+++ net/get-avg-time.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo -n `date '+%Y-%m-%d %H:%M:%S'`
+echo -n ' '
+
 h=192.168.1.1
-ping -w 40 -q -n $h | grep rtt | cut -d '=' -f 2 | cut -d '/' -f 2
+echo `ping -w 40 -q -n $h | grep rtt | cut -d '=' -f 2 | cut -d '/' -f 2`
 

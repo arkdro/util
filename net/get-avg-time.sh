1dad76f7619fbc54235c3198cc8b23de967c9125
Arkadiy Drovosekov <arkdro@gmail.com>
2014-07-23 10:49:10 +0400

    locks, logs, two set variables

--- net/get-avg-time.sh
+++ net/get-avg-time.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-echo -n `date '+%Y-%m-%d %H:%M:%S'`
-echo -n ' '
+set -eu
 
-h=192.168.1.1
-echo `ping -w 40 -q -n $h | grep rtt | cut -d '=' -f 2 | cut -d '/' -f 2`
+run(){
+	echo -n `date '+%Y-%m-%d %H:%M:%S'`
+	echo -n ' '
 
+	h=192.168.1.1
+	echo `ping -w 40 -q -n $h | grep rtt | cut -d '=' -f 2 | cut -d '/' -f 2`
+}
+
+b=`basename "$0"`
+LOCK=/tmp/$b-$LOGNAME.lck
+test -e $LOCK && exit 0
+
+touch $LOCK
+acc=${1:-"$HOME/${b}.log"}
+run >> "$acc"
+rm -f -- $LOCK

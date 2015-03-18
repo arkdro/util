6e983d7ffa60c42d7526f55d732ea2d6a8a50b14
arkdro <arkdro@gmail.com>
2015-03-19 00:23:36 +0300

    short output

--- disk/temperature.sh
+++ disk/temperature.sh
@@ -2,4 +2,4 @@
 
 # requires hddtemp on port 7634
 
-telnet localhost 7634 2>/dev/null | grep '/dev/sda' | cut -d '|' -f 4
+telnet localhost 7634 2>/dev/null | grep '/dev/sda' | cut -d '|' -f 4 | cut -c -5

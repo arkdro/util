e6f62164e1a81949c9e95b6cb2d14b9b33622413
arkdro <arkdro@gmail.com>
2015-03-19 00:23:05 +0300

    change xrandr

--- x/xr1.sh
+++ x/xr1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # xrandr --output HDMI1 --off
-xrandr --output HDMI1 --right-of LVDS1 --mode 1920x1080
+xrandr --output LVDS1 --mode 1366x768 --output HDMI1 --right-of LVDS1 --mode 1920x1080

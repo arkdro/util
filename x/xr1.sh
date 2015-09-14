2937ff7066a47e4216eb0460d8f0717af0d55b86
arkdro <arkdro@gmail.com>
2015-09-14 20:08:07 +0300

    add black color set

--- x/xr1.sh
+++ x/xr1.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# set black color to be really black
+# xrandr --output HDMI1 --set "Broadcast RGB" "Full"
+
 # xrandr --output HDMI1 --off
 xrandr --output LVDS1 --mode 1366x768 --output HDMI1 --right-of LVDS1 --mode 1920x1080

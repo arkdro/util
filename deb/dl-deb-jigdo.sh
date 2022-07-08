38996fe8c9af113d9a0a5667c63bdebd2353083d
Arkadii Drovosekov <arkdro@protonmail.com>
2022-07-08 21:36:10 +0200

    update jigdo download

--- deb/dl-deb-jigdo.sh
+++ deb/dl-deb-jigdo.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
+renice -n 18 -p $$
+ionice -c 3 -p $$
+
+cur_dir="`dirname "$0"`"
+cur_base="`basename "$0"-$$`"
+log="${cur_dir}/${cur_base}.log"
+
 default_dist="/opt/debian/7.4.0"
 dist=${1:-"$default_dist"}
 
-for a in *.jigdo
-do
-	jigdo-lite --noask --scan "$dist" $a
-done
+arch="amd64"
+base="debian-11.3.0-${arch}-DVD"
+first=1
+last=19
 
-for a in *.jigdo
-do
-	b=`basename $a .jigdo`
-	echo $b
-	fn="${b}.iso"
-	gpg --print-mds --with-colons "$fn" > "${fn}.sums"
-done
+for a in `seq $first $last`
+	do
+		date >> "$log"
+		b="$base-$a"
+		echo "b: $b" >> "$log"
+		jigdo-lite --noask --scan "$dist" "$b.jigdo"
+		echo "rc=$?"
+		( gpg --print-mds --with-colon $b.iso > $b.iso.sums & )
+	done
+

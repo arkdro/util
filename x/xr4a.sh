e6dc95b19f049bf2de280393e5585c7028ec2b00
Arkadii Drovosekov <arkdro@protonmail.com>
2024-01-26 20:10:02 +0100

    add xrandr

---
+++ x/xr4a.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# set black color to be really black
+# xrandr --output HDMI1 --set "Broadcast RGB" "Full"
+
+# xrandr --output HDMI1 --off
+
+# The idea here is to increase the screen resolution virtually (because we are limited to 1366x768 physically) the command would be (replace screen-name):
+# xrandr --output screen-name --mode 1366x768 --panning 1640x922 --scale 1.20058565x1.20208604
+
+
+xrandr \
+	--output eDP \
+	--mode 1920x1080 \
+	--panning 3840x2160 \
+	--scale 2x2 \

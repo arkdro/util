bbc828fba1c754a74944a0dacbd141110c8c6037
Arkadii Drovosekov <arkdro@protonmail.com>
2024-07-27 19:52:25 +0200

    loop over variable instead of duplication

--- net/ufw.sh
+++ net/ufw.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-ufw allow in on lo to any proto tcp port 22
-ufw deny in to any proto tcp port 22
-ufw allow in on lo to any proto tcp port 25
-ufw deny in to any proto tcp port 25
-ufw allow in on lo to any proto tcp port 631
-ufw deny in to any proto tcp port 631
-ufw allow in on lo to any proto udp port 631
-ufw deny in to any proto udp port 631
-ufw allow in on lo to any proto tcp port 8080
-ufw deny in to any proto tcp port 8080
 
-# ufw allow in on tun0 to any proto udp port 57489
-# ufw allow in on tun0 to any proto tcp port 57489
+one_rule() {
+	local port="$1"
+	ufw allow in on lo to any proto tcp port "$port"
+	ufw deny in to any proto tcp port "$port"
+}
+
+# 1716 - kde connect
+ports="22 25 631 1716 8080"
+
+for port in $ports
+do
+	one_rule "$port"
+done
 
 # to not pollute logs, uncomment '& stop' in /etc/rsyslog.d/20-ufw.conf

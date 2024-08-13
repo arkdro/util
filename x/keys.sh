b0e30fd75ec367bdb45d34720cbb7fcc8224cce5
Arkadii Drovosekov <arkdro@protonmail.com>
2024-08-13 11:23:37 +0200

    keyboard keys

--- x/keys.sh
+++ x/keys.sh
@@ -3,3 +3,10 @@
 # f%$#ed marketing has made left shift short and added '<>' key near it.
 # so make this '<>' key Shift_L
 xmodmap -e "keycode 94 = Shift_L"
+
+# Muhenkan on JIS keyboard
+xmodmap -e 'keycode 102 = Alt_L'
+
+# '\' on JIS keyboard. Make it '/'
+xmodmap -e 'keycode 97 = slash question'
+
--- x/xset.sh
+++ x/xset.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 xset b off r rate 250 30
+
+# '<>' key Shift_L
 xmodmap -e 'keycode 94 = Shift_L'
+
+# Muhenkan on JIS keyboard
+xmodmap -e 'keycode 102 = Alt_L'
+
+# '\' on JIS keyboard. Make it '/'
+xmodmap -e 'keycode 97 = slash question'
+
+pgrep -u $USER xbindkeys > /dev/null || xbindkeys

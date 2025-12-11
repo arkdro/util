0fe6e4c558285e7e64a427f01fef3c286becde54
Arkadii Drovosekov <arkdro@protonmail.com>
2025-12-11 12:51:29 +0100

    add a separator and sort

--- sums/sums.sh
+++ sums/sums.sh
@@ -4,13 +4,13 @@ one_hash() {
 	local hash="$1"
 	local file="$2"
 	openssl dgst "-${hash}" "$file" |\
-		awk -v in_file="$file" -v in_hash="$hash" "{printf(\"%s:%s:%s\n\", in_file, in_hash, \$NF)}"
+		awk -v in_file="$file" -v in_hash="$hash" "{printf(\"%s:%s:%s:\n\", in_file, in_hash, \$NF)}"
 }
 
 one_file() {
 	local file="$1"
 	size=`stat -c '%s' "$file"`
-	echo "$file:0_size:$size"
+	echo "$file:0_size:$size:"
 	while read hash
 	do
 		one_hash "$hash" "$file" &
@@ -24,9 +24,11 @@ one_file() {
 	EOF
 }
 
+export LC_ALL=C
+
 find "$@" \( -type f -o -type l \) |\
 	while read f
 	do
-		one_file "$f"
+		one_file "$f" | sort
 		wait
 	done

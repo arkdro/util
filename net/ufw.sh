c11511c25e36b154be70043b22d8a84286a692fd
Arkadii Drovosekov <arkdro@protonmail.com>
2026-03-19 21:30:36 +0100

    split rules for local and network interfaces

--- net/ufw.sh
+++ net/ufw.sh
@@ -6,12 +6,33 @@ one_rule() {
 	ufw deny in to any proto tcp port "$port"
 }
 
-# 1716 - kde connect
-ports="22 25 631 1716 8080"
+one_rule_e() {
+	local port="$1"
+	ufw allow in on lo to any proto tcp port "$port"
+	ufw allow in on en+ to any proto tcp port "$port"
+	ufw deny in to any proto tcp port "$port"
+}
 
-for port in $ports
-do
-	one_rule "$port"
-done
+set_rules() {
+	for port in $ports
+	do
+		one_rule "$port"
+	done
+}
+
+set_rules_e() {
+	for port in $ports_e
+	do
+		one_rule_e "$port"
+	done
+}
+
+# 1716 - kde connect
+# 4713 - pipewire
+ports="25 631 1716 4713 8080"
+ports_e="22"
+
+set_rules
+set_rules_e
 
 # to not pollute logs, uncomment '& stop' in /etc/rsyslog.d/20-ufw.conf
